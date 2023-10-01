3cd8c4a9cb66fa0d112785baa798e2c797b64b36
William Pereira <MetalHexx@outlook.com>
2023-10-01 17:24:47 -0400

    Connection button is now disabled if we're already connected.

---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Helpers/BooleanInverterConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TeensyRom.Ui.Helpers
+{
+    /// <summary>
+    /// This convertor will take a boolean value and inverse it for the UI xaml
+    /// </summary>
+    public class BooleanInverterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+            return Binding.DoNothing;
+        }
+    }
+}
