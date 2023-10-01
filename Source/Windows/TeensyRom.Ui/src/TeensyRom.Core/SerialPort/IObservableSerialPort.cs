44e66475736065414ee64f01dc1b2ee6f9409c99
William Pereira <MetalHexx@outlook.com>
2023-10-01 01:52:28 -0400

    Completed some todos and a little cleanup

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/SerialPort/IObservableSerialPort.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/SerialPort/IObservableSerialPort.cs
@@ -20,7 +20,6 @@ namespace TeensyRom.Core.Serial
 
         //TOOD: Add an observable value representing a stream of changes to the status of the serial port connection
         //TODO: Add an observable value representing a stream of data received from the serial port
-        //TODO: Add a method to Set the selected port state
         //TODO: Add a method to open the port using the selected port
         //TODO: Add a method to close the port
         //TODO: Add a method to send data
--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Features/Connect/ConnectViewModel.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Features/Connect/ConnectViewModel.cs
@@ -21,10 +21,6 @@ namespace TeensyRom.Ui.Features.Connect
         [Reactive]
         public string Logs { get; set; } = string.Empty;
 
-        //TODO: Add a property to represent the selected port in the drop down
-
-        //TODO: Add a command to set the serial port to the users selection
-
         //TODO: Add a command to connect to the selected serial port
 
         //TODO: Add a property to display the logs received from the serial port
@@ -35,6 +31,7 @@ namespace TeensyRom.Ui.Features.Connect
         public ConnectViewModel(IObservableSerialPort serialService)
         {
             _serialService = serialService;
+
             _serialService.Ports.ToPropertyEx(this, vm => vm.Ports);
 
             this.WhenAnyValue(x => x.SelectedPort)
