22198ac5d649294f714459c6ca0f5936ff1764c6
William Pereira <MetalHexx@outlook.com>
2023-11-28 00:57:54 -0500

    Added a test for CopyFileCommand.

---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Tests/Integration/CopyFileCommandTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeensyRom.Core.Commands;
+using TeensyRom.Core.Logging;
+using TeensyRom.Core.Serial;
+using TeensyRom.Core.Settings;
+
+namespace TeensyRom.Tests.Integration
+{
+    [Collection("SerialPortTests")]
+    public class CopyFileCommandTests : IDisposable
+    {
+        private readonly TeensyFixture _fixture;
+
+        public CopyFileCommandTests()
+        {
+            _fixture = new TeensyFixture();
+        }
+
+        [Fact]
+        public void Given_FileExists_When_CommandCalled_FileCopiedSuccessfully()
+        {
+            //Arrange
+            var testId = Guid.NewGuid().ToString().Substring(0, 5);
+            var sourceFilePath = "/integration-test-files/test-existing-folder/test-file.sid";
+            var destParentPath = $"/integration-test-files/copy-dir-{testId}/";
+            var destFilePath = $"{destParentPath}test-file-{testId}.sid";
+
+            _fixture.Initialize(initOpenPort: true);
+
+            //Act
+            _fixture.CopyFileCommand.Execute(sourceFilePath, destFilePath);
+            Thread.Sleep(100);
+            var directory = _fixture.GetDirectoryContentCommand.Execute(destParentPath, 0, 1);
+
+            //Assert
+            directory.Files.First().Path.Should().Be(destFilePath);
+        }
+
+        public void Dispose()
+        {
+            _fixture.Dispose();
+        }
+    }
+}
