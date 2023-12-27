7eebbaf053c9dd0b5ce1aa40dca0b7ab8625870d
William Pereira <MetalHexx@outlook.com>
2023-12-27 12:16:49 -0500

    Added Files view title

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Features/Files/FilesViewModel.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Features/Files/FilesViewModel.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeensyRom.Ui.Helpers.ViewModel;
 
 namespace TeensyRom.Ui.Features.Files
 {
-    public class FilesViewModel
+    public class FilesViewModel : FeatureViewModelBase
     {
+        public FilesViewModel()
+        {
+            FeatureTitle = "Files";
+        }
     }
 }
