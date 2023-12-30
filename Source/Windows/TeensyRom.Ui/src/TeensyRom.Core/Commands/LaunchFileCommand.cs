42aa5394f61e9a11ea9ab6bd6d117ae4ba54f4d3
William Pereira <MetalHexx@outlook.com>
2023-12-30 02:38:10 -0500

    Cleaned up logging and error handling

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Commands/LaunchFileCommand.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Commands/LaunchFileCommand.cs
@@ -21,34 +21,20 @@ namespace TeensyRom.Core.Commands.File.LaunchFile
 
         public Task<LaunchFileResponse> Handle(LaunchFileCommand request, CancellationToken cancellationToken)
         {
-            try
+            _serialPort.SendIntBytes(TeensyConstants.Launch_File_Token, 2);
+
+            if (!GetAck())
             {
-                _logService.Log($"Sending launch file token: {TeensyConstants.Launch_File_Token}");
-                _serialPort.SendIntBytes(TeensyConstants.Launch_File_Token, 2);
-
-                if (!GetAck())
-                {
-                    ReadSerialAsString();
-                    throw new TeensyException("Error getting acknowledgement when Launch File Token sent");
-                }
-
-                _logService.Log($"Sending SD_nUSB: {TeensyConstants.Sd_Card_Token}");
-                _serialPort.SendIntBytes(GetStorageToken(_settings.TargetType), 1);
-
-                _logService.Log($"Sending file launch request path: {request.Path}");
-                _serialPort.Write($"{request.Path}\0");
-
-                if (!GetAck())
-                {
-                    ReadSerialAsString(msToWait: 100);
-                    throw new TeensyException("Error getting acknowledgement when launch path sent");
-                }
-                _logService.Log("Launch file request complete!");
+                ReadSerialAsString();
+                throw new TeensyException("Error getting acknowledgement when Launch File Token sent");
             }
-            catch (TeensyException ex)
+            _serialPort.SendIntBytes(GetStorageToken(_settings.TargetType), 1);
+            _serialPort.Write($"{request.Path}\0");
+
+            if (!GetAck())
             {
-                _logService.Log($"Error launching file.  {ex.Message}");
-                return Task.FromResult(new LaunchFileResponse());
+                ReadSerialAsString(msToWait: 100);
+                throw new TeensyException("Error getting acknowledgement when launch path sent");
             }
             return Task.FromResult(new LaunchFileResponse());
         }
