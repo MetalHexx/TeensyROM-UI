f843bf5e882a24cd7c8e3dc4cb1fc01b7b3d8d5b
William Pereira <MetalHexx@outlook.com>
2023-12-29 23:12:33 -0500

    Updated ping command with better response.

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Commands/PingCommand.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Commands/PingCommand.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using System.ComponentModel;
 using System.Reactive;
 using TeensyRom.Core.Logging;
 using TeensyRom.Core.Serial;
@@ -6,25 +7,26 @@ using TeensyRom.Core.Settings;
 
 namespace TeensyRom.Core.Commands
 {
-    public class PingCommand : IRequest { }
+    public class PingCommand : IRequest<PingResult> { }
+    public class PingResult : CommandResult { }
 
-    public class PingCommandHandler : TeensyCommand, IRequestHandler<PingCommand>
+    public class PingCommandHandler : TeensyCommand, IRequestHandler<PingCommand, PingResult>
     {
         public PingCommandHandler(ISettingsService settingsService, IObservableSerialPort serialPort, ILoggingService logService)
             : base(settingsService, serialPort, logService) { }
 
-        public Task Handle(PingCommand request, CancellationToken cancellationToken)
+        public Task<PingResult> Handle(PingCommand request, CancellationToken cancellationToken)
         {
             if (!_serialPort.IsOpen)
             {
-                _logService.Log("You must first connect in order to ping the device.");
-                return Task.CompletedTask;
+                return Task.FromResult(new PingResult 
+                {
+                    Error = "You must first connect in order to ping the device."
+                });
             }
-            _logService.Log($"Pinging device");
-
             _serialPort.Write(TeensyConstants.Ping_Bytes.ToArray(), 0, 2);
 
-            return Task.CompletedTask;
+            return Task.FromResult(new PingResult());
         }
     }
 }
