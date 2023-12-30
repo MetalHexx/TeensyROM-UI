2eae3e1f7eb47564a9b7e31d70c4808310b28d30
William Pereira <MetalHexx@outlook.com>
2023-12-29 23:11:18 -0500

    Updated CommandResult to be a class.

---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Commands/CommandResult.cs
@@ -0,0 +1,8 @@
+﻿namespace TeensyRom.Core.Commands
+{
+    public class CommandResult
+    {
+        public bool IsSuccess { get; set; } = true;
+        public string Error = string.Empty;
+    }
+}
