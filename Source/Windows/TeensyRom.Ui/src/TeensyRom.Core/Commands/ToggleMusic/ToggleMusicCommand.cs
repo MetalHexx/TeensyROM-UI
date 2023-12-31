e969e9c3c909a12e9bcd43805fa04035ee1d8b20
William Pereira <MetalHexx@outlook.com>
2023-12-31 01:36:15 -0500

    Moved to classes to their own files

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Commands/ToggleMusic/ToggleMusicCommand.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Commands/ToggleMusic/ToggleMusicCommand.cs
@@ -1,27 +1,10 @@
 ﻿using MediatR;
 using System.Drawing;
 using System.Reactive;
-using TeensyRom.Core.Common;
 using TeensyRom.Core.Logging;
-using TeensyRom.Core.Serial;
 using TeensyRom.Core.Settings;
 
 namespace TeensyRom.Core.Commands
 {
-    public class ToggleMusicCommand() : IRequest<ToggleMusicResponse>;
-    public class ToggleMusicResponse() : CommandResult;
-    public class ToggleMusicHandler(IObservableSerialPort _serialPort) :  IRequestHandler<ToggleMusicCommand, ToggleMusicResponse>
-    {
-        public Task<ToggleMusicResponse> Handle(ToggleMusicCommand request, CancellationToken cancellationToken)
-        {
-            _serialPort.SendIntBytes(TeensyToken.PauseMusic, 2);
-
-            if (_serialPort.GetAck() != TeensyToken.Ack)
-            {
-                _serialPort.ReadSerialAsString(msToWait: 100);
-                throw new TeensyException("Error getting acknowledgement when pause music token sent");
-            }
-            return Task.FromResult(new ToggleMusicResponse());
-        }
-    }
+    public class ToggleMusicCommand() : IRequest<ToggleMusicResult>;
 }
---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Commands/ToggleMusic/ToggleMusicHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using TeensyRom.Core.Common;
+using TeensyRom.Core.Serial;
+
+namespace TeensyRom.Core.Commands
+{
+    public class ToggleMusicHandler(IObservableSerialPort _serialPort) :  IRequestHandler<ToggleMusicCommand, ToggleMusicResult>
+    {
+        public Task<ToggleMusicResult> Handle(ToggleMusicCommand request, CancellationToken cancellationToken)
+        {
+            _serialPort.SendIntBytes(TeensyToken.PauseMusic, 2);
+
+            if (_serialPort.GetAck() != TeensyToken.Ack)
+            {
+                _serialPort.ReadSerialAsString(msToWait: 100);
+                throw new TeensyException("Error getting acknowledgement when pause music token sent");
+            }
+            return Task.FromResult(new ToggleMusicResult());
+        }
+    }
+}
