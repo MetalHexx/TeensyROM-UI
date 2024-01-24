68283d2a1134d660cf1e16fd8a7073d3a25ad50c
William Pereira <MetalHexx@outlook.com>
2024-01-23 23:05:11 -0500

    Added dialog and cache clear on settings save.

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Features/Settings/SettingsViewModel.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Features/Settings/SettingsViewModel.cs
@@ -1,15 +1,18 @@
 ﻿using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using System;
+using System.ComponentModel;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Threading;
 using TeensyRom.Core.Common;
 using TeensyRom.Core.Logging;
 using TeensyRom.Core.Settings;
+using TeensyRom.Core.Storage.Services;
 using TeensyRom.Ui.Helpers.ViewModel;
 using TeensyRom.Ui.Services;
 
@@ -17,41 +20,35 @@ namespace TeensyRom.Ui.Features.Settings
 {
     public class SettingsViewModel: FeatureViewModelBase, IDisposable
     {
-        [Reactive]
-        public string Logs { get; set; } = string.Empty;
-
-        [ObservableAsProperty]
-        public TeensySettings Settings { get; set; }
+        [Reactive] public string Logs { get; set; } = string.Empty;
+        [ObservableAsProperty] public bool IsDirty { get; }
+        [ObservableAsProperty] public TeensySettings Settings { get; set; }
+        public Interaction<string, bool> ConfirmSave { get; } = new Interaction<string, bool>();
 
         public ReactiveCommand<Unit, Unit> SaveSettingsCommand { get; set; }
 
         private readonly ISettingsService _settingsService;
+        private readonly ICachedStorageService _storage;
+        private readonly ISnackbarService _alert;
+        private readonly IDialogService _dialog;
         private readonly ILoggingService _logService;
         private readonly IDisposable _logsSubscription;
         private readonly StringBuilder _logBuilder = new StringBuilder();
 
-        public SettingsViewModel(ISettingsService settings, ISnackbarService alert, ILoggingService logService)
+        public SettingsViewModel(ISettingsService settings, ICachedStorageService storage, ISnackbarService alert, IDialogService dialog, ILoggingService logService)
         {
             FeatureTitle = "Settings";
 
             _logService = logService;
-            _settingsService = settings;            
-
+            _settingsService = settings;
+            _storage = storage;
+            _alert = alert;
+            _dialog = dialog;
             _settingsService.Settings.ToPropertyEx(this, vm => vm.Settings);
 
-            SaveSettingsCommand = ReactiveCommand.Create<Unit, Unit>(n => 
-            {
-                var success = _settingsService.SaveSettings(Settings);
-                if (success)
-                {
-                    alert.Enqueue("Settings saved successfully.");
-                }
-                else
-                {
-                    alert.Enqueue("Error saving settings");
-                }
-                return Unit.Default;
-            }, outputScheduler: ImmediateScheduler.Instance);
+            SaveSettingsCommand = ReactiveCommand.CreateFromTask<Unit, Unit>(
+                execute: async n => await HandleSave(),
+                outputScheduler: RxApp.MainThreadScheduler);
 
             _logsSubscription = _logService.Logs
                 .Where(log => !string.IsNullOrWhiteSpace(log))
@@ -61,6 +58,25 @@ namespace TeensyRom.Ui.Features.Settings
                 {
                     Logs = logs;
                 });
+        }
+
+        private async Task<Unit> HandleSave()
+        {
+            var confirmed = await _dialog.ShowConfirmation("Are you sure you want to save settings?\r\rWarning: Cache will be deleted if you proceed.");
+
+            if (!confirmed) return Unit.Default;
+
+            var success = _settingsService.SaveSettings(Settings);
+            if (success)
+            {
+                _alert.Enqueue("Settings saved successfully.");
+                _storage.ClearCache();
+            }
+            else
+            {
+                _alert.Enqueue("Error saving settings");
+            }
+            return Unit.Default;
         }
 
         public void Dispose()
