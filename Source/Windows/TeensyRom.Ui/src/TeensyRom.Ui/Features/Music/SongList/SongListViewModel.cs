f9d15f2a79048ae0794227c577d037c02483ca5c
William Pereira <MetalHexx@outlook.com>
2024-01-25 23:30:36 -0500

    Song list now shows a confirmation dialog before delete

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Features/Music/SongList/SongListViewModel.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Features/Music/SongList/SongListViewModel.cs
@@ -6,13 +6,16 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 using TeensyRom.Core.Commands;
+using TeensyRom.Core.Logging;
 using TeensyRom.Core.Serial;
 using TeensyRom.Core.Settings;
 using TeensyRom.Core.Storage.Entities;
 using TeensyRom.Ui.Features.Common.Models;
 using TeensyRom.Ui.Features.Music.State;
 using TeensyRom.Ui.Features.NavigationHost;
+using TeensyRom.Ui.Services;
 
 namespace TeensyRom.Ui.Features.Music.SongList
 {
@@ -25,12 +28,16 @@ namespace TeensyRom.Ui.Features.Music.SongList
         public ReactiveCommand<DirectoryItem, Unit> LoadDirectoryCommand { get; set; }
 
         private readonly IMusicState _musicState;
+        private readonly IAlertService _alert;
+        private readonly IDialogService _dialog;
         private IDisposable _directoryTreeSubscription;
 
-        public SongListViewModel(IMusicState musicState)
+        public SongListViewModel(IMusicState musicState, IAlertService alert, IDialogService dialog)
         {
             _musicState = musicState;
-           
+            _alert = alert;
+            _dialog = dialog;
+
             PlayCommand = ReactiveCommand.CreateFromTask<SongItem, bool>(song =>
                 musicState.LoadSong(song), outputScheduler: RxApp.MainThreadScheduler);
 
@@ -39,12 +46,26 @@ namespace TeensyRom.Ui.Features.Music.SongList
                 outputScheduler: RxApp.MainThreadScheduler);
 
             DeleteCommand = ReactiveCommand.CreateFromTask<FileItem>(
-                musicState.DeleteFile, outputScheduler: RxApp.MainThreadScheduler);
+                execute: HandleDelete, 
+                outputScheduler: RxApp.MainThreadScheduler);
 
             LoadDirectoryCommand = ReactiveCommand.CreateFromTask<DirectoryItem>(directory =>
                 musicState.LoadDirectory(directory.Path), outputScheduler: RxApp.MainThreadScheduler);
 
             _musicState.DirectoryContent.ToPropertyEx(this, x => x.DirectoryContent);
+        }
+
+        private async Task<Unit> HandleDelete(FileItem fileItem)
+        {
+            var confirmed = await _dialog.ShowConfirmation($"Are you sure you want to delete {fileItem.Path}?");
+
+            if (!confirmed) return Unit.Default;
+
+            var success = _musicState.DeleteFile(fileItem);
+
+            _alert.Publish($"{fileItem.Path} has been deleted.");
+
+            return Unit.Default;
         }
         public void Dispose()
         {
