48f1318367123d9b6dec6f4e45afbb58b2e9f059
William Pereira <MetalHexx@outlook.com>
2024-02-29 17:50:23 -0500

    Fixed a bug where /hsvc was considered part of the base path, but not all users will have that path.

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Music/Sid/SidConstants.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Music/Sid/SidConstants.cs
@@ -5,6 +5,6 @@
         public const string Hvsc = "HVSC";
         public const string DeepSid = "DeepSID";
         public const string Musician_Image_Local_Path = @"Music\Images\Composers\";
-        public const string Hvsc_Musician_Base_Remote_Path = @"/hvsc/MUSICIANS/";
+        public const string Hvsc_Musician_Base_Remote_Path = @"/MUSICIANS/";
     }
 }
