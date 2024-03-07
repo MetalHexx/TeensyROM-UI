b191d60d7ba46e30599267a5c5e4de7efe550ff5
William Pereira <MetalHexx@outlook.com>
2024-03-06 22:54:09 -0500

    Added some custom chrome.

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Features/NavigationHost/NavigationHostWindow.xaml.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Features/NavigationHost/NavigationHostWindow.xaml.cs
@@ -9,11 +9,13 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Shell;
 using TeensyRom.Ui.Features.NavigationHost;
 
 namespace TeensyRom.Ui.Main
@@ -26,6 +28,8 @@ namespace TeensyRom.Ui.Main
         public MainWindow()
         {
             InitializeComponent();
+
+            StateChanged += MainWindowStateChangeRaised;
 
             MessageBus.Current.Listen<NavAnimationMessage>().Subscribe(message =>
                 {
@@ -40,7 +44,54 @@ namespace TeensyRom.Ui.Main
                         default:
                             break;
                     }
-                });
+            });
+        }
+
+        // Can execute
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        // Minimize
+        private void CommandBinding_Executed_Minimize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        // Maximize
+        private void CommandBinding_Executed_Maximize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+
+        // Restore
+        private void CommandBinding_Executed_Restore(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.RestoreWindow(this);
+        }
+
+        // Close
+        private void CommandBinding_Executed_Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        // State change
+        private void MainWindowStateChangeRaised(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                MainWindowBorder.BorderThickness = new Thickness(8);
+                RestoreButton.Visibility = Visibility.Visible;
+                MaximizeButton.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                MainWindowBorder.BorderThickness = new Thickness(0);
+                RestoreButton.Visibility = Visibility.Collapsed;
+                MaximizeButton.Visibility = Visibility.Visible;
+            }
         }
     }
 }
