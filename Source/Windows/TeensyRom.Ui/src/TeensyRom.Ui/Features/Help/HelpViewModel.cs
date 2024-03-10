9202ae4638afb6b741418a36e49fc558c0ed18d3
William Pereira <MetalHexx@outlook.com>
2024-03-09 23:55:48 -0500

    Updated help view with new resources and working hyperlink buttons.

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Features/Help/HelpViewModel.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Features/Help/HelpViewModel.cs
@@ -1,13 +1,63 @@
 ﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System.Diagnostics;
+using System.Security.Policy;
+using TeensyRom.Ui.Controls.FeatureTitle;
 using TeensyRom.Ui.Helpers.ViewModel;
 
 namespace TeensyRom.Ui.Features.Help
 {
-    public class HelpViewModel : FeatureViewModelBase
+    public class HelpViewModel
     {
+        public ReactiveCommand<Unit, Unit> NavigateToUiGithubCommand { get; }
+        public ReactiveCommand<Unit, Unit> NavigateToTrGithubCommand { get; }
+        public ReactiveCommand<Unit, Unit> NavigateToDiscordCommand { get; }
+        public ReactiveCommand<Unit, Unit> NavigateToEbayCommand { get; }
+        public ReactiveCommand<Unit, Unit> NavigateToTindieCommand { get; }        
+        public ReactiveCommand<Unit, Unit> NavigateToOneLoad64Command { get; }
+        public ReactiveCommand<Unit, Unit> NavigateToHvscCommand { get; }
+        public ReactiveCommand<Unit, Unit> NavigateToSidListCommand { get; }
+        public ReactiveCommand<Unit, Unit> NavigateToDeepSidCommand { get; }
+
+
         public HelpViewModel() 
         {
-            FeatureTitle = "File Transfer";
+            NavigateToUiGithubCommand = ReactiveCommand.Create(() => 
+            {
+                Process.Start(new ProcessStartInfo("https://github.com/MetalHexx/TeensyROM-UI") { UseShellExecute = true });
+            });
+            NavigateToTrGithubCommand = ReactiveCommand.Create(() => 
+            {
+                Process.Start(new ProcessStartInfo("https://github.com/SensoriumEmbedded/TeensyROM") { UseShellExecute = true });
+            });
+            NavigateToDiscordCommand = ReactiveCommand.Create(() =>
+            {
+                Process.Start(new ProcessStartInfo("https://discord.gg/ubSAb74S5U") { UseShellExecute = true });
+            });
+            NavigateToEbayCommand = ReactiveCommand.Create(() => 
+            {
+                Process.Start(new ProcessStartInfo("https://www.ebay.com/usr/travster1") { UseShellExecute = true });
+            });
+            NavigateToTindieCommand = ReactiveCommand.Create(() =>
+            {
+                Process.Start(new ProcessStartInfo("https://www.tindie.com/products/travissmith/teensyrom-cartridge-for-c64128/") { UseShellExecute = true });
+            });
+            NavigateToOneLoad64Command = ReactiveCommand.Create(() =>
+            {
+                Process.Start(new ProcessStartInfo("https://www.youtube.com/watch?v=lz0CJbkplj0&list=PLmN5cgEuNrpiCj1LfKBDUZS06ZBCjif5b") { UseShellExecute = true });
+            });
+            NavigateToHvscCommand = ReactiveCommand.Create(() =>
+            {
+                Process.Start(new ProcessStartInfo("https://www.hvsc.c64.org/") { UseShellExecute = true });
+            });
+            NavigateToSidListCommand = ReactiveCommand.Create(() =>
+            {
+                Process.Start(new ProcessStartInfo("https://www.transbyte.org/SID/SIDlist.html") { UseShellExecute = true });
+            });
+            NavigateToDeepSidCommand = ReactiveCommand.Create(() =>
+            {
+                Process.Start(new ProcessStartInfo("https://github.com/Chordian/deepsid") { UseShellExecute = true });
+            });
         }
     }
 }
---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Helpers/Converters/StringToPackIconKindConverter.cs
@@ -0,0 +1,29 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TeensyRom.Ui.Helpers
+{
+    public class StringToPackIconKindConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string iconName && !string.IsNullOrEmpty(iconName))
+            {
+                if (Enum.TryParse<PackIconKind>(iconName, out var iconKind))
+                {
+                    return iconKind;
+                }
+            }
+
+            // Return a default icon if the input is null/empty or not a valid icon name
+            return PackIconKind.Alert; // Choose an appropriate fallback icon
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
