8f031b0da12a4dc0f8d9a0547714524ea2320fa8
William Pereira <MetalHexx@outlook.com>
2024-03-11 00:47:14 -0400

    fix: Updated the cache all message to be less verbose and removed text for fetching game images.

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Controls/CornerToolbar/CornerToolbarViewModel.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Controls/CornerToolbar/CornerToolbarViewModel.cs
@@ -31,7 +31,7 @@ namespace TeensyRom.Ui.Controls.CornerToolbar
             CacheAllCommand = ReactiveCommand.CreateFromTask(
                 execute: async _ =>
                 {
-                    var confirm = await dialog.ShowConfirmation($"Cache All \r\rCreates a local index of all the files stored on your {storageType} storage.  This will enable rich discovery of music and programs though search and file launch randomization.\r\rThis may take a few minutes if you have a lot of files from libraries like OneLoad64 or HSVC on your {storageType} storage.\r\rIf you specified game art folders in your settings, it may take a few extra minutes to download those images from the TR.\r\rIt's worth the wait. Proceed?");
+                    var confirm = await dialog.ShowConfirmation($"Cache All \r\rCreates a local index of all the files stored on your {storageType} storage.  This will enable rich discovery of music and programs though search and file launch randomization.\r\rThis may take a few minutes if you have thousands of files. Proceed?");
                     if (!confirm) return;
 
                     await cacheAllFunc();
