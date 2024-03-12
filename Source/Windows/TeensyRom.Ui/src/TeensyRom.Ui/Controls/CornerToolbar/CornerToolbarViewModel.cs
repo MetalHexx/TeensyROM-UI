fcf714f874e3e6e8432b9e29fab995221f2529d1
William Pereira <MetalHexx@outlook.com>
2024-03-11 23:23:13 -0400

    fix: wording change on cache all.

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Controls/CornerToolbar/CornerToolbarViewModel.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Controls/CornerToolbar/CornerToolbarViewModel.cs
@@ -31,7 +31,7 @@ namespace TeensyRom.Ui.Controls.CornerToolbar
             CacheAllCommand = ReactiveCommand.CreateFromTask(
                 execute: async _ =>
                 {
-                    var confirm = await dialog.ShowConfirmation($"This will creates a local index of all the files stored on your {storageType}.\r\r This will enable search and file launch randomization to span your entire collection.\r\rThis may take a few minutes if you have thousands of files. Proceed?");
+                    var confirm = await dialog.ShowConfirmation($"This will create a local index of all the files stored on your {storageType} to enable search and file launch randomization across your entire collection.\r\rThis may take a few minutes if you have thousands of files. Proceed?");
                     if (!confirm) return;
 
                     await cacheAllFunc();
