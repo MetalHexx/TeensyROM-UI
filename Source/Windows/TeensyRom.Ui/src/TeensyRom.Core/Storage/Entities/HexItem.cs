e0a6de69a9a23624a3898621048b46a0264d4513
William Pereira <MetalHexx@outlook.com>
2024-03-31 23:39:55 -0400

    chore: New Hex Item type

---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Storage/Entities/HexItem.cs
@@ -0,0 +1,24 @@
+﻿
+namespace TeensyRom.Core.Storage.Entities
+{
+    public class HexItem : FileItem, ILaunchableItem, IViewableItem
+    {
+        public List<ViewableItemImage> Images { get; init; } = [];
+        public override HexItem Clone() => new()
+        {
+            Name = Name,
+            Path = Path,
+            Size = Size,
+            IsFavorite = IsFavorite,
+            Title = Title,
+            Creator = Creator,
+            ReleaseInfo = ReleaseInfo,
+            Description = Description,
+            ShareUrl = ShareUrl,
+            MetadataSource = MetadataSource,
+            Meta1 = Meta1,
+            Meta2 = Meta2,
+            SourcePath = SourcePath
+        };
+    }
+}
