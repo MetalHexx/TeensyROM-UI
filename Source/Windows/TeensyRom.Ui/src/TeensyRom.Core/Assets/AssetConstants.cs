70d393699a1f200f2ea6a11ffebea0b31838a4cb
William Pereira <MetalHexx@outlook.com>
2024-03-31 23:38:49 -0400

    chore: TR assets for hex file type

---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Assets/AssetConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeensyRom.Core.Assets
+{
+    public class AssetConstants
+    {
+        public const string SensoriumLogoFilePath = @"Assets\Other\Sensorium.png";       
+        public const string TeensyRomHardwareFilePath = @"Assets\Other\TeensyROM-Hardware-Purple.png";
+    }
+}
