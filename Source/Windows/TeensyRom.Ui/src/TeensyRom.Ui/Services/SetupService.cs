d512f21f7d8dff2c94430c92dd88d1563baf77d8
William Pereira <MetalHexx@outlook.com>
2024-04-14 17:25:57 -0400

    fix: re-enabled the wizard caching

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Services/SetupService.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Services/SetupService.cs
@@ -200,7 +200,7 @@ namespace TeensyRom.Ui.Services
 
             _settings.StorageType = TeensyStorageType.SD;
             _settingsService.SaveSettings(_settings);
-            //await _storage.CacheAll(StorageConstants.Remote_Path_Root);
+            await _storage.CacheAll(StorageConstants.Remote_Path_Root);
 
             await OnCacheUSB();
         }
@@ -215,7 +215,7 @@ namespace TeensyRom.Ui.Services
             }
             _settings.StorageType = TeensyStorageType.USB;
             _settingsService.SaveSettings(_settings);
-            //await _storage.CacheAll(StorageConstants.Remote_Path_Root);
+            await _storage.CacheAll(StorageConstants.Remote_Path_Root);
 
             result = await _dialog.ShowConfirmation("File Indexing Completed", $"Now that your file information has been indexed, lets head over to the Discover view and do some exploring.");
 
