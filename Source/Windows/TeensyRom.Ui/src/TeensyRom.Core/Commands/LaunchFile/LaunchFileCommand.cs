a6f1ca6c28cc7652c6daa1116450b2b80e7d0d56
William Pereira <MetalHexx@outlook.com>
2024-07-14 18:48:14 -0400

    chore: added fields to the LaunchFileCommand for better logging

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Commands/LaunchFile/LaunchFileCommand.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Commands/LaunchFile/LaunchFileCommand.cs
@@ -8,6 +8,8 @@ namespace TeensyRom.Core.Commands.File.LaunchFile
 {
     public class LaunchFileCommand(TeensyStorageType storageType, ILaunchableItem item) : IRequest<LaunchFileResult>
     {
+        public string Path => LaunchItem.Path;
+        public long Size => LaunchItem.Size;
         public TeensyStorageType StorageType { get; } = storageType;
         public ILaunchableItem LaunchItem { get; } = item;
     }
