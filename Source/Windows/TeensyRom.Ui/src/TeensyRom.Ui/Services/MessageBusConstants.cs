732099279f25dd842effd2cce6b958d1208dba84
metalhexx <metalhexx@outlook.com>
2025-02-16 03:59:52 -0500

    feat: added app global keyboard shortcuts for +-50% speed, +- 1%, default speed, and num pad voice mutes

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Services/MessageBusConstants.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Ui/Services/MessageBusConstants.cs
@@ -11,6 +11,15 @@ namespace TeensyRom.Ui.Services
         /// <summary>
         /// Message used to signal when the 1, 2, 3 keys are typed in for muting sid voices.
         /// </summary>
-        public const string GlobalVoiceKeyMessageName = "GlobalVoiceKey";
+        public const string SidVoiceMuteKeyPressed = "SidVoiceKeyPressed";
+        public const string SidSpeedIncreaseKeyPressed = "SidSpeedIncreaseKeyPressed";
+        public const string SidSpeedDecreaseKeyPressed = "SidSpeedDecreaseKeyPressed";
+        public const string SidSpeedIncrease50KeyPressed = "SidSpeedIncrease50KeyPressed";
+        public const string SidSpeedDecrease50KeyPressed = "SidSpeedDecrease50KeyPressed";
+        public const string SidSpeedDefaultKeyPressed = "SidSpeedDefaultKeyPressed";
+        public const string PlayerPlayPausedKeyPressed = "PlayerPlayPausedKeyPressed";
+        public const string PlayerStopKeyPressed = "PlayerStopKeyPressed";
+        public const string PlayerNextTrackKeyPressed = "PlayerNextTrackKeyPressed";
+        public const string PlayerPreviousTrackKeyPressed = "PlayerPreviousTrackKeyPressed";
     }
 }
