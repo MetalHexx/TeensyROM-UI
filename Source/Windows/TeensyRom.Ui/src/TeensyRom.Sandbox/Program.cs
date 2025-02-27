5d15e4ee6d6f12cb0fcb69dafa39ad53ce92c11b
metalhexx <metalhexx@outlook.com>
2025-02-26 23:27:51 -0500

    chore: added a sandbox project for little experiments to keep handy.

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Sandbox/Program.cs
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Sandbox/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Management;
 
+//Gets all the serial devices in the system and prints their info
 var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_SerialPort");
 foreach (ManagementObject port in searcher.Get())
 {
