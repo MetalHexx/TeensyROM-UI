37fd795be6eafd49cb6e65d715fd9efb16330c91
metalhexx <metalhexx@outlook.com>
2025-03-15 22:18:02 -0400

    refactor: new interface around StorageCache to prepare for potential backend database change.

---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Core/Storage/Services/IStorageCache.cs
@@ -0,0 +1,24 @@
+﻿
+using TeensyRom.Core.Settings;
+using TeensyRom.Core.Storage.Entities;
+
+namespace TeensyRom.Core.Storage.Services
+{
+    public interface IStorageCache
+    {
+        void DeleteDirectory(string path);
+        void DeleteDirectoryWithChildren(string path);
+        void DeleteFile(string path);
+        StorageCacheItem EnsureParents(string path);
+        StorageCacheItem? GetByDirPath(string path);
+        List<IFileItem> GetFileByName(string name);
+        IFileItem? GetFileByPath(string filePath);
+        void SetBanLists(List<string> bannedFolders, List<string> bannedFiles);
+        void UpsertDirectory(string path, StorageCacheItem directory);
+        void UpsertFile(IFileItem fileItem);
+        void Clear();
+        void EnsureFavorites(List<string> favPaths);
+        IEnumerable<ILaunchableItem> Search(string searchText, List<string> favPaths, List<string> stopSearchWords, SearchWeights searchWeights, params TeensyFileType[] fileTypes);
+        ILaunchableItem? GetRandomFile(StorageScope scope, string scopePath, params TeensyFileType[] fileTypes);
+    }
+}
