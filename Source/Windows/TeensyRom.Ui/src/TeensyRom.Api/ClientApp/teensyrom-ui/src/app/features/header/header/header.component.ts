1b16fcb3d0a012baf8191ea19cbb23ad1f83acc2
metalhexx <metalhexx@outlook.com>
2025-05-18 23:01:54 -0400

    chore: added header and main terminal view

---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-ui/src/app/features/header/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-ui/src/app/features/terminal-view/terminal-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terminal-view',
+  imports: [],
+  templateUrl: './terminal-view.component.html',
+  styleUrl: './terminal-view.component.scss'
+})
+export class TerminalViewComponent {
+
+}
