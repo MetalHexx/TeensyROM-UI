bd05306d1a9964fc60157e7ab4873f326986f5d6
metalhexx <metalhexx@outlook.com>
2025-05-21 23:27:10 -0400

    chore: layout cleanup

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-ui/src/app/shell/layout/layout.component.ts
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-ui/src/app/shell/layout/layout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
