825051401ad45758c65ac8b52a3c494a3011c446
metalhexx <metalhexx@outlook.com>
2025-05-21 22:29:02 -0400

    chore: added basic header component

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-ui/src/app/shell/layout/layout.component.ts
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-ui/src/app/shell/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
