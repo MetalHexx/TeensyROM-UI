a4b619f6d139b01bd50c2afcb2aba61f73697ceb
metalhexx <metalhexx@outlook.com>
2025-05-24 00:39:44 -0400

    chore: added placeholder player view

---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-ui/src/app/features/player/player-view/player-view.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jukebox-view',
+  standalone: true,
+  templateUrl: './player-view.component.html',
+  styleUrls: ['./player-view.component.scss'],
+})
+export class PlayerViewComponent {}
