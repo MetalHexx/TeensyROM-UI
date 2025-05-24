79816a00cf443947a642e7f4149614b5e011b5dc
metalhexx <metalhexx@outlook.com>
2025-05-24 00:40:13 -0400

    chore: added placeholder settings view

---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-ui/src/app/features/settings/settings-view.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-view',
+  standalone: true,
+  templateUrl: './settings-view.component.html',
+  styleUrls: ['./settings-view.component.scss'],
+})
+export class SettingsViewComponent {}
