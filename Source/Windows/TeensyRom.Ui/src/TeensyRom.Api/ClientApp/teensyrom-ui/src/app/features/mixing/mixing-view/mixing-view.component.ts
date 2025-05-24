71c6f5a576df1cf1937dd5658e48ea355c53a78c
metalhexx <metalhexx@outlook.com>
2025-05-24 00:39:27 -0400

    chore: added placeholder mixer view

---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-ui/src/app/features/mixing/mixing-view/mixing-view.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mixing-view',
+  standalone: true,
+  templateUrl: './mixing-view.component.html',
+  styleUrls: ['./mixing-view.component.scss'],
+})
+export class MixingViewComponent {}
