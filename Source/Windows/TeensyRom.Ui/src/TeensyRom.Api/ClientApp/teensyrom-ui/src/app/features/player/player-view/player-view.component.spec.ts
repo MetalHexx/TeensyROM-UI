88086c7d9cec13174e14fba6fb22171f82b1b979
metalhexx <metalhexx@outlook.com>
2025-05-26 21:14:51 -0400

    test: added basic tests for feature view components

---
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-ui/src/app/features/player/player-view/player-view.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PlayerViewComponent } from './player-view.component';
+
+describe('PlayerViewComponent', () => {
+  let component: PlayerViewComponent;
+  let fixture: ComponentFixture<PlayerViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PlayerViewComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PlayerViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
