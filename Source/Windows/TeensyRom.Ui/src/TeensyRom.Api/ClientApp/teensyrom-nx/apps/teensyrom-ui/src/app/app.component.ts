729388a9a781d3354841e04daaa2782a5317ec40
metalhexx <metalhexx@outlook.com>
2025-05-27 19:03:17 -0400

    feat(ui): add new Angular library with component and testing setup

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/apps/teensyrom-ui/src/app/app.component.ts
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/apps/teensyrom-ui/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/apps/teensyrom-ui/src/app/app.routes.ts
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/apps/teensyrom-ui/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'devices',
+        loadComponent: () => import('@teensyrom-nx/ui').then(m => m.UiComponent)
+    }
+];
