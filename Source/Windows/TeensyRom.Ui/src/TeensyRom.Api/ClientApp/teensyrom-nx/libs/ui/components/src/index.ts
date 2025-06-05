087ea1901f70e4e97d988bc4928745bdafa6308c
metalhexx <metalhexx@outlook.com>
2025-06-04 23:08:18 -0400

    chore: removed ui.component and moved menu-item one folder up

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/ui/components/src/index.ts
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/ui/components/src/index.ts
@@ -1,5 +1,4 @@
-export * from './lib/ui/ui.component';
-export * from './lib/ui/menu-item/menu-item.component';
-export * from './lib/ui/menu-item/menu-item.model';
+export * from './lib/menu-item/menu-item.component';
+export * from './lib/menu-item/menu-item.model';
 export * from './lib/icon-label/icon-label.component';
 export * from './lib/status-icon-label/status-icon-label.component';
