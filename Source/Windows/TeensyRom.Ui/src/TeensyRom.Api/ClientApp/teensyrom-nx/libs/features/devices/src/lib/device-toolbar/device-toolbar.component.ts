710e8189f1938d464de40697f41cb1748d677eef
metalhexx <metalhexx@outlook.com>
2025-06-28 16:42:52 -0400

    chore: messing around with toolbars

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/features/devices/src/lib/device-toolbar/device-toolbar.component.ts
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/features/devices/src/lib/device-toolbar/device-toolbar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { IconLabelComponent } from '@teensyrom-nx/ui/components';
 
 @Component({
   selector: 'lib-device-toolbar',
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatExpansionModule, MatIconModule, IconLabelComponent],
   templateUrl: './device-toolbar.component.html',
   styleUrl: './device-toolbar.component.css',
 })
