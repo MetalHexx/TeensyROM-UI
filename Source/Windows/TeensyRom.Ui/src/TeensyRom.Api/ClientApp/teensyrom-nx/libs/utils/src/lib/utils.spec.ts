115813fb68e354c27f1864b26cd35222c5d3194d
metalhexx <metalhexx@outlook.com>
2025-09-18 20:48:25 -0400

    chore: removed sample utils.ts

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/utils/src/lib/utils.spec.ts
+++
@@ -1,7 +0,0 @@
-import { placeholder } from './utils';
-
-describe('utils', () => {
-  it('should work', () => {
-    expect(placeholder()).toEqual('utils library initialized');
-  });
-});
--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/utils/src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-// Placeholder file for @teensyrom-nx/utils library
-// Utilities will be added in subsequent phases
-
-export function placeholder(): string {
-  return 'utils library initialized';
-}
