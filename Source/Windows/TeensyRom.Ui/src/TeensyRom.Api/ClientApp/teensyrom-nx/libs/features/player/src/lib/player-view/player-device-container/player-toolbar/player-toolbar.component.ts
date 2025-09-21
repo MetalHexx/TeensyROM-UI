4be12b1999e1ad1aca2566adc4d7258e649aebce
metalhexx <metalhexx@outlook.com>
2025-09-20 22:26:15 -0400

    feat: updated search and player toolbar components with our new compact card layout component.

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/features/player/src/lib/player-view/player-device-container/player-toolbar/player-toolbar.component.ts
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/features/player/src/lib/player-view/player-device-container/player-toolbar/player-toolbar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
+import { CompactCardLayoutComponent } from '@teensyrom-nx/ui/components';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'lib-player-toolbar',
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, CompactCardLayoutComponent, MatButtonModule, MatIconModule],
   templateUrl: './player-toolbar.component.html',
   styleUrl: './player-toolbar.component.scss',
 })
--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/features/player/src/lib/player-view/player-device-container/storage-container/search-toolbar/search-toolbar.component.ts
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/features/player/src/lib/player-view/player-device-container/storage-container/search-toolbar/search-toolbar.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
+import { CompactCardLayoutComponent } from '@teensyrom-nx/ui/components';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'lib-search-toolbar',
-  imports: [CommonModule, MatCardModule, MatInputModule, MatFormFieldModule, MatIconModule],
+  imports: [
+    CommonModule,
+    CompactCardLayoutComponent,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+  ],
   templateUrl: './search-toolbar.component.html',
   styleUrl: './search-toolbar.component.scss',
 })
