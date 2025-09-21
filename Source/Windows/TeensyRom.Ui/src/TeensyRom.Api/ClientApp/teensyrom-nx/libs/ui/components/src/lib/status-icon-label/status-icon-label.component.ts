5ef8d11d53559dbff3e62cd03e542ebb8b8950f7
metalhexx <metalhexx@outlook.com>
2025-09-21 14:26:17 -0400

    chore: cleanup

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/ui/components/src/lib/status-icon-label/status-icon-label.component.ts
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/ui/components/src/lib/status-icon-label/status-icon-label.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { IconLabelComponent } from '../icon-label/icon-label.component';
