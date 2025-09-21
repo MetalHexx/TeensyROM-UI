0d8d2b546370031835fcda8beaf30f6b795ebec3
metalhexx <metalhexx@outlook.com>
2025-09-21 14:25:33 -0400

    feat: updated search input to use new reusable

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/features/player/src/lib/player-view/player-device-container/storage-container/search-toolbar/search-toolbar.component.ts
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/features/player/src/lib/player-view/player-device-container/storage-container/search-toolbar/search-toolbar.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CompactCardLayoutComponent } from '@teensyrom-nx/ui/components';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
+import { CompactCardLayoutComponent, InputFieldComponent } from '@teensyrom-nx/ui/components';
 
 @Component({
   selector: 'lib-search-toolbar',
-  imports: [
-    CommonModule,
-    CompactCardLayoutComponent,
-    MatInputModule,
-    MatFormFieldModule,
-    MatIconModule,
-  ],
+  imports: [CommonModule, CompactCardLayoutComponent, InputFieldComponent],
   templateUrl: './search-toolbar.component.html',
   styleUrl: './search-toolbar.component.scss',
 })
