e303dcbb9a3fee8ecf560b9ba85c5afed6ff340f
metalhexx <metalhexx@outlook.com>
2025-10-07 00:06:40 -0400

    feat: file info now shows file type when creator is unavailable

--- Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/features/player/src/lib/player-view/player-device-container/player-toolbar/file-info/file-info.component.ts
+++ Source/Windows/TeensyRom.Ui/src/TeensyRom.Api/ClientApp/teensyrom-nx/libs/features/player/src/lib/player-view/player-device-container/player-toolbar/file-info/file-info.component.ts
@@ -20,4 +20,47 @@ export class FileInfoComponent {
     }
     return item.images.map(img => img.url);
   });
+
+  // Get friendly file type name from meta1 field
+  fileTypeName = computed(() => {
+    const meta1 = this.fileItem()?.meta1?.toLowerCase();
+    if (!meta1) return '';
+
+    switch (meta1) {
+      case 'sid':
+        return 'Music';
+      case 'crt':
+        return 'Cartridge';
+      case 'prg':
+        return 'Program';
+      case 'p00':
+        return 'Program';
+      case 'hex':
+        return 'Machine Code';
+      case 'kla':
+        return 'Koala Image';
+      case 'koa':
+        return 'Koala Image';
+      case 'art':
+        return 'Art Studio Image';
+      case 'aas':
+        return 'Art Studio Image';
+      case 'hpi':
+        return 'HiRes Image';
+      case 'd64':
+        return 'Disk Image';
+      case 'seq':
+        return 'Sequential File';
+      case 'txt':
+        return 'Text File';
+      case 'zip':
+        return 'Archive';
+      case 'nfo':
+        return 'Info File';
+      case 'unknown':
+        return 'Unknown';
+      default:
+        return meta1.toUpperCase();
+    }
+  });
 }
